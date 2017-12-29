54ca0ff833e5d3b9f847c48892126670cde9dcc8
yusufafini <yusufafini@outlook.com>
2017-12-29 17:37:51 +0530

    flask app not working but still committed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Flaskr.flaskr',
+    packages=['Flaskr.flaskr'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
